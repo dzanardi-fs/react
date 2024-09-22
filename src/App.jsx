dc607eb442bcfe0e518534d2b7f586ea64f1b2b9
dzanardi <dzanardi@siu.edu.ar>
2024-09-22 00:35:30 -0300

    Primeros cambios en un archivo App.js de React

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const welcome = {
+  greeting : 'Hey',
+  title : 'React'
+};
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function getTitle(title){
+  return title;
+}
+//const myElement = <h1>Hello {welcome.title}</h1>;
+const myElement = React.createElement('h1',null,`Hello ${welcome.title}`);
+
+function App() {
+  return(
+    <div>
+        <h1>Hello {welcome.greeting} {welcome.title}!!!</h1>
+        <h1>Hello {getTitle('React (función)')}!!!</h1>
+        <label htmlFor='search'>Search: </label>
+        <input id='search' type='text' />
+        <hr/>
+        {myElement}
+    </div>
+  );
 }
 
 export default App
