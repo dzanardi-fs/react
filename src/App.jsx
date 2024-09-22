9858ce7e1ca85cbbaf8b6cebbf4b028343e2c53b
dzanardi <dzanardi@siu.edu.ar>
2024-09-22 09:41:26 -0300

    feat: elementos html en funciones y llamados desde app de forma jerarquica

--- src/App.jsx
+++ src/App.jsx
@@ -26,27 +26,44 @@ function App() {
     <>
       <div>
         <h1>My Hacker Stories</h1>
-        <label htmlFor="search">Search: </label>
-        <input id="search" type="text" />
-        <hr />
-        <ul>
-          {list.map(function (item, index) {
-            return (
-              <li key={item.objectID} >
-              <span> {index}</span>
-                <span>
-                  <a href={item.url}>{item.title}</a>
-                </span>
-                <span>{item.author}</span>
-                <span>{item.num_comments}</span>
-                <span>{item.points}</span>
-              </li>
-            );
-          })}
-        </ul>
+        <Search />
+        
+                <hr />
+
+        <List />
+
       </div>
     </>
   )
+}
+
+function Search() {
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+    </div>
+  );
+}
+
+
+function List() {
+  return (
+    <ul>
+      {list.map(function (item) {
+        return (
+          <li key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </li>
+        );
+      })}
+    </ul>
+  );
 }
 
 export default App
